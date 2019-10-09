547b0c2306b41e1a8177a056c3fc585f760a6d6d
Pavel <pavel.aplevich@gmail.com>
2019-10-09 14:49:06 +0300

    И еще один

--- Training Project/src/JavaTutorOneLoopsTwo.java
+++ Training Project/src/JavaTutorOneLoopsTwo.java
@@ -1,9 +1,17 @@
 public class JavaTutorOneLoopsTwo {
+
+    /*
+    Циклы.
+    Задание №2.
+    Вычислить значения функции на отрезке [a, b] с шагом h:
+            y = { x, x > 2;
+              = {-x, x <=2.
+    */
     public static void main(String[] args) {
     }
 
-    public static void LoopsTaskTwo (double a, double b, double h){
-        for(double i = a; i <= b; i=i+h){
+    public static void LoopsTaskTwo (double a, double b, double h){ //Метод выводит значения функции на данном отрезке
+        for(double i = a; i <= b; i=i+h){                           //с заданным шагом
             if (i > 2){
                 System.out.println("y = " + i);
             } else {
