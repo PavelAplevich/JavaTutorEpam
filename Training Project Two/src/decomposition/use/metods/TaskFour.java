20c59b354caaf2f0ef91b80977a8be60928320a8
pavel <dariaapril@MacBook-Air-Daria.local>
2019-10-14 02:39:24 +0300

    1

--- Training Project Two/src/decomposition/use/metods/TaskFour.java
+++ Training Project Two/src/decomposition/use/metods/TaskFour.java
@@ -1,5 +1,4 @@
 package decomposition.use.metods;
-// TODO: 13.10.2019  
 
     /*
     Декомпозиция с использованием методов(подпрограммы).
@@ -9,4 +8,22 @@ package decomposition.use.metods;
     */
 
 public class TaskFour {
+    public static void main(String[] args) {
+        //Т.к. не указан метод ввода, оставил заккоментированный хард-код, использованный мной для проверки.
+
+        int[][] pointArray = {{1,2} ,{1,4} ,{-1,6} ,{-2, -7} ,{-13, 9}};
+        printPairPoint(pointArray);
+    }
+
+    public static void printPairPoint(int[][] pointArray){
+        int[][] array = new int[2][2];
+        array = maxPairPoint(pointArray);
+        System.out.print("Наибольшее расстояние между точками ");
+        System.out.printf("%d,%d и %d,%d", array[0][0], array[0][1], array[1][0], array[1][1]);
+
+    }
+
+    public static int[][] maxPairPoint(int[][] pointArray){
+        // TODO: 10/14/19 Продолжить здесь
+    }
 }
