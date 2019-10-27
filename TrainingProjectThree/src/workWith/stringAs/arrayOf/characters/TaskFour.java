96256afbe04a5481d44ecf319f01ca1d6350b7fe
Pavel <pavel.aplevich@gmail.com>
2019-10-27 21:44:31 +0300

    comm

--- TrainingProjectThree/src/workWith/stringAs/arrayOf/characters/TaskFour.java
+++ TrainingProjectThree/src/workWith/stringAs/arrayOf/characters/TaskFour.java
@@ -10,25 +10,24 @@ public class TaskFour {
     public static void main(String[] args) {
         //Т.к. не указан метод ввода, я оставлю закомментированный хардкод, использованный мной для проверки
 
-//        String str = "12.3//a..3 4..wase.t56hdfh5..qwq//.j.fj./.....,,wte4";
-//        System.out.println("Количество чисел в строке: " + countNumberInString(str));
+        String str = "12.3//a..3 4..wase.t56hdfh5..qwq//.j.fj./.....,,wte4";
+        System.out.println("Количество чисел в строке: " + countNumberInString(str));
     }
 
     //Метод возвращает количество чисел в сроке
     public static int countNumberInString(String str){
         int count = 0;
-        //Регулярное выражение убирает все нецифры кроме точек
-        str = str.replaceAll("[^\\. && \\D]", " ");//
-        //Регулярное выражение убирает все подряд идущие точки
-        str = str.replaceAll("\\.{2,}",  " ");
-        //Выражение убирает все точки перед которыми ничего нет
-        str = str.replaceAll("\\D\\.", " ");
-        //Выражение убирает все множественные пробелы, заменяя их одним
-        str = str.replaceAll("\\ {2,}", " ");
-        //Используем метод split для образования массива строк, содержащих числа
-        String[] array = str.split(" ");
-        //Возвращаем длину массива, которая и является искомым количеством чисел
-        return array.length;
+        char[] array = str.toCharArray();
+        for(int i = 0; i < array.length;i++){
+            if(isNumeral(array[i])){
+                for(int j)
+            }
+        }
+        return count;
+    }
+
+    public static boolean isNumeral(char numeral){
+        return numeral >= 48 && numeral <= 57;
     }
 
 }
